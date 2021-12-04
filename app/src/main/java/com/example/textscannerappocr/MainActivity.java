47a9d3bb5da9114080a6c8d7da61176fa2988067
RabiusRaju <rabiussani8121@gmail.com>
2021-12-04 12:43:08 +0600

    first commit

---
+++ app/src/main/java/com/example/textscannerappocr/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.textscannerappocr;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button_capture,button_copy;
+    TextView text_data;
+    private static final int REQUEST_CAMERA_CODE = 100;
+    Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_capture = findViewById(R.id.button_capture);
+        button_copy = findViewById(R.id.button_copy);
+        text_data = findViewById(R.id.text_data);
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.CAMERA
+            },REQUEST_CAMERA_CODE);
+        }
+
+        button_capture.setOnClickListener(v -> CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(MainActivity.this));
+
+        button_copy.setOnClickListener(v -> {
+            String scanner_text = text_data.getText().toString();
+            copyToClipBoard(scanner_text);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if(resultCode == RESULT_OK){
+                Uri resultUri = result.getUri();
+                try{
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
+                    getTextFromImage(bitmap);
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    private void getTextFromImage(Bitmap bitmap){
+        TextRecognizer recognizer = new TextRecognizer.Builder(this).build();
+        if(!recognizer.isOperational()){
+            Toast.makeText(MainActivity.this, "Error Occured!!",Toast.LENGTH_LONG);
+        }else{
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> textBlockSparseArray = recognizer.detect(frame);
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i= 0 ; i < textBlockSparseArray.size(); i++){
+                TextBlock textBlock = textBlockSparseArray.valueAt(i);
+                stringBuilder.append(textBlock.getValue());
+                stringBuilder.append("\n");
+            }
+            text_data.setText(stringBuilder.toString());
+            button_capture.setText("Retake");
+            button_capture.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void copyToClipBoard(String text){
+        ClipboardManager clipBoard = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("Cliped data", text);
+        clipBoard.setPrimaryClip(clip);
+        Toast.makeText(MainActivity.this,"Copied to Clipboard", Toast.LENGTH_LONG);
+    }
+}
